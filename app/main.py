60be2366a293d12ec3d56f9767846b20aea3abf9
Animish Nishakar <105592560+AnimishNishakar@users.noreply.github.com>
2026-02-06 18:10:29 +0530

    Update home route message with personalized greeting

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from DevOps End-to-End Project 🚀"
+    return "Hello! ANIMISH NISHAKAR 🚀 This is DevOps End-to-End Project 🚀"
 
 @app.route("/health")
 def health():
@@ -12,3 +12,4 @@ def health():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+
